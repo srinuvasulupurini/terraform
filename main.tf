1eeaa2e0d11c7ae2bf9def47d41caa223953c638
root <tfuser@rmail.com>
2023-02-23 00:57:24 +0000

    by user2

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ resource aws_instance "aws_new_inst_s3"{
 	ami = "ami-01bb8868a249be5ca"
 	instance_type = "t2.micro"
 	tags = {
-		name = "new-s3-instance"
+		name = "new-s3-instance_user2"
 	}
 }
