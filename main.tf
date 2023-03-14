5963bc7d182a4c23bdb27c692935261f86b72116
root <tfuser@rmail.com>
2023-03-14 03:25:34 +0000

    adding main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 provider "aws"{
-	region = "ap-northeast-1"
-}
-terraform{
-	backend "s3" {
-		bucket = "terraform-dhana-bucket"
-		key = "terraform.tfstate"
 		region = "ap-northeast-1"
-}
-}
-
-resource aws_instance "aws_new_inst_s3"{
-	ami = "ami-01bb8868a249be5ca"
-	instance_type = "t2.micro"
-	tags = {
-		name = "new-s3-instance_user2"
 	}
+
+terraform {
+  backend "s3" {
+		bucket = "tfrevision-s3bucket"
+		key = "terraform"
+		region = "ap-northeast-1"
+	}
+}
+resource aws_instance "new_s3_instance"{
+		ami = "ami-05375ba8414409b07"
+		instance_type = "t2.micro"
+		count = 1
+		tags = {
+		name = "new_s3_instance_${count.index}"
+          }		
+
 }
